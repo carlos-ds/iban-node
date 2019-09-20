9b357008a1132269ffdec3d54776a0e05e7fa604
karel <karel.de.smet@outlook.com>
2019-09-20 20:56:44 +0200

    Created new branches and added environment variables

--- index.js
+++ index.js
@@ -1,44 +1,52 @@
-const iban = require('./js/iban.js');
-const mysql = require('mysql');
-const express = require('express');
-const ejs = require('ejs');
-const path = require('path');
+const iban = require("./js/iban.js");
+const mysql = require("mysql");
+const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 const pool = mysql.createPool({
-    connectionLimit: 100,
-    host: 'localhost',
-    user: 'root',
-    password: process.argv[2],
-    database: 'personal',
-    debug: false
-  });
-
-app.get('/', function (req, res) {
-    pool.query('SELECT * FROM iban ORDER BY id desc LIMIT 10', function(err, result, fields){
-        if(err) {
-            return res.json({'error': true, 'message': err});
-        }
-
-        res.render('index', { data: result });
-    });
+	connectionLimit: 100,
+	host: process.env.DATABASE_HOSTNAME,
+	user: process.env.DATABASE_USERNAME,
+	password: process.env.DATABASE_PASSWORD,
+	database: process.env.DATABASE_NAME,
+	debug: false
 });
 
-app.get('/create', function(req, res) {
-    let newIban = iban.generate();
-    pool.query(`INSERT INTO iban (iban) VALUES (?)`, [newIban], function(err, result, fields){
-        if(err) {
-            return res.json({'error': true, 'message': err});
-        }
+app.get("/", function(req, res) {
+	pool.query("SELECT * FROM iban ORDER BY id desc LIMIT 10", function(
+		err,
+		result,
+		fields
+	) {
+		if (err) {
+			return res.json({ error: true, message: err });
+		}
 
-        res.redirect('/');
-    });
+		res.render("index", { data: result });
+	});
+});
+
+app.get("/create", function(req, res) {
+	let newIban = iban.generate();
+	pool.query(`INSERT INTO iban (iban) VALUES (?)`, [newIban], function(
+		err,
+		result,
+		fields
+	) {
+		if (err) {
+			return res.json({ error: true, message: err });
+		}
+
+		res.redirect("/");
+	});
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- js/iban.test.js
+++ js/iban.test.js
@@ -1,6 +1,6 @@
 const iban = require('./iban.js');
-var generatedIban = iban.generate();
-var ibanRegEx = /^BE\d{2}(\s\d{4}){3}$/;
+const generatedIban = iban.generate();
+const ibanRegEx = /^BE\d{2}(\s\d{4}){3}$/;
 
  test('iban is 19 characters long', () => {
      expect(generatedIban.length).toEqual(19);
