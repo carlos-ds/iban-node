2f14baa79ba5e0f367803d526ccccdcb0290eabc
karel <karel.de.smet@outlook.com>
2020-05-01 22:22:51 +0200

    Added a new route for the validation logic

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const mysql = require("mysql");
 const express = require("express");
 const ejs = require("ejs");
 const path = require("path");
-
+const bodyParser = require("body-parser");
+const { body } = require('express-validator');
+const session = require("express-session");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true, cookie: { secure: true } }));
 
 const pool = mysql.createPool({
 	connectionLimit: 100,
@@ -21,17 +27,18 @@ const pool = mysql.createPool({
 });
 
 app.get("/", function (req, res) {
+	const validate = req.session.validate ? req.session.validate : null;
 	pool.query(`SELECT * FROM ${process.env.DATABASE_NAME}.bank_account ORDER BY id desc LIMIT 10`, function (err, result, fields) {
 		if (err) {
 			return res.json({ error: true, message: err });
 		}
 
-		res.render("index", { data: result });
+		res.render("index", { data: result, validate });
 	});
 });
 
 app.get("/create", function (req, res) {
-	let newIban = iban.generate();
+	let newIban = iban.generateIban();
 	pool.query(`INSERT INTO ${process.env.DATABASE_NAME}.bank_account (value, format, country) VALUES (?, ?, ?)`, [newIban, "IBAN", "BE"], function (err, result, fields) {
 		if (err) {
 			return res.json({ error: true, message: err });
@@ -39,6 +46,12 @@ app.get("/create", function (req, res) {
 
 		res.redirect("/");
 	});
+});
+
+app.post("/validate", (req, res) => {
+	req.session.validate = iban.validate(req.body.iban);
+	console.log(req.session.validate);
+	res.redirect("/");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
