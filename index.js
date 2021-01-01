a391093e4485fc630dddea81b48a14c21ff02fd4
Karel De Smet <karel.de.smet@outlook.com>
2021-01-01 15:38:52 +0100

    Added CORS header for alternative URL

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
   } else {
     res.setHeader("Access-Control-Allow-Origin", process.env.APPLICATION_URL);
+    res.setHeader("Access-Control-Allow-Origin", process.env.HEROKU_APPLICATION_URL);
   }
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader("Vary", "Origin");
