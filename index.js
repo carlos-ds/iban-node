add93ee04ac5abac17f9f2dcf3d352b214a60622
Karel De Smet <karel.de.smet@outlook.com>
2021-01-24 22:54:47 +0100

    Wrapped all routes in connection callback to fix the CORS bug

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const username = encodeURIComponent(process.env.DATABASE_USERNAME);
 const password = encodeURIComponent(process.env.DATABASE_PASSWORD);
 const uri = `mongodb+srv://${username}:${password}@${process.env.DATABASE_HOSTNAME}/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 client.connect().then(() => {
   app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Methods", "GET, POST");
@@ -34,58 +35,59 @@ client.connect().then(() => {
   app.use(express.json());
   // From the official Express documentation: "If you don’t want to use Helmet, then at least disable the X-Powered-By header. Attackers can use this header (which is enabled by default) to detect apps running Express and then launch specifically-targeted attacks."
   app.disable("x-powered-by");
-  app.listen(port, () => console.log(`Port: ${port}\nEnvironment: ${environment}`));
-});
 
-app.get("/", async function (req, res, next) {
-  try {
-    let limit = parseInt(req.query.limit, 10);
+  app.get("/", async function (req, res, next) {
+    try {
+      let limit = parseInt(req.query.limit, 10);
 
-    if (!limit || limit <= 0) {
-      limit = 1;
+      if (!limit || limit <= 0) {
+        limit = 1;
+      }
+
+      const collection = client.db(process.env.DATABASE_NAME).collection("iban");
+      const result = collection
+        .find()
+        .sort({ createdAt: -1 })
+        .limit(limit)
+        .toArray((error, documents) => {
+          if (error) {
+            return res.status(500).json(error);
+          }
+          return res.status(200).json(documents);
+        });
+    } catch (err) {
+      return res.status(500).json(err);
     }
+  });
 
-    const collection = client.db(process.env.DATABASE_NAME).collection("iban");
-    const result = collection
-      .find()
-      .sort({ createdAt: -1 })
-      .limit(limit)
-      .toArray((error, documents) => {
-        if (error) {
-          return res.status(500).json(error);
-        }
-        return res.status(200).json(documents);
-      });
-  } catch (err) {
-    return res.status(500).json(err);
-  }
-});
+  app.get("/create", async function (req, res) {
+    try {
+      let accountNumber = iban.generateIban();
+      const collection = client.db(process.env.DATABASE_NAME).collection("iban");
+      const document = {
+        accountNumber: accountNumber,
+        createdAt: new Date().toISOString(),
+        createdBy: "generate",
+      };
+      const result = await collection.insertOne(document);
 
-app.get("/create", async function (req, res) {
-  try {
-    let accountNumber = iban.generateIban();
-    const collection = client.db(process.env.DATABASE_NAME).collection("iban");
-    const document = {
-      accountNumber: accountNumber,
-      createdAt: new Date().toISOString(),
-      createdBy: "generate",
-    };
-    const result = await collection.insertOne(document);
+      console.log(`# documents inserted: ${result.insertedCount}`);
 
-    console.log(`# documents inserted: ${result.insertedCount}`);
+      return res.redirect("/?limit=5");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  });
 
-    return res.redirect("/?limit=5");
-  } catch (err) {
-    return res.status(500).json(err);
-  }
-});
+  app.post("/validate", (req, res) => {
+    try {
+      let validation = iban.validate(req.body.accountNumber);
+      return res.status(200).json(validation);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).send(err);
+    }
+  });
 
-app.post("/validate", (req, res) => {
-  try {
-    let validation = iban.validate(req.body.accountNumber);
-    return res.status(200).json(validation);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).send(err);
-  }
+  app.listen(port, () => console.log(`Port: ${port}\nEnvironment: ${environment}`));
 });
