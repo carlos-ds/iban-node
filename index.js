31a2f55525610e67c67f6bff2fddfa59eb84bce1
Karel De Smet <karel.de.smet@outlook.com>
2021-01-24 20:54:50 +0100

    Swapped MySQL for MongoDB

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 require("dotenv").config();
 
 const iban = require("./js/iban.js");
-const mysql = require("mysql");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const config = require("./config.js");
 const port = process.env.PORT || 3306;
 const environment = process.env.NODE_ENV || "dev";
+
+// MongoDB connection
+const MongoClient = require("mongodb").MongoClient;
+const username = encodeURIComponent(process.env.DATABASE_USERNAME);
+const password = encodeURIComponent(process.env.DATABASE_PASSWORD);
+const uri = `mongodb+srv://${username}:${password}@${process.env.DATABASE_HOSTNAME}/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect().then(() => {
+  app.listen(port, () => console.log(`Port: ${port}\nEnvironment: ${environment}`));
+});
 
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST");
@@ -30,40 +39,47 @@ app.use(express.json());
 // From the official Express documentation: "If you don’t want to use Helmet, then at least disable the X-Powered-By header. Attackers can use this header (which is enabled by default) to detect apps running Express and then launch specifically-targeted attacks."
 app.disable("x-powered-by");
 
-const pool = mysql.createPool({
-  connectionLimit: 100,
-  host: process.env.DATABASE_HOSTNAME,
-  user: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  debug: false,
-});
+app.get("/", async function (req, res, next) {
+  try {
+    let limit = parseInt(req.query.limit, 10);
 
-app.get("/", function (req, res, next) {
-  let limit = parseInt(req.query.limit, 10);
+    if (!limit || limit <= 0) {
+      limit = 1;
+    }
 
-  if (!limit || limit <= 0) {
-    limit = 1;
+    const collection = client.db(process.env.DATABASE_NAME).collection("iban");
+    const result = collection
+      .find()
+      .sort({ createdAt: -1 })
+      .limit(limit)
+      .toArray((error, documents) => {
+        if (error) {
+          return res.status(500).json(error);
+        }
+        return res.status(200).json(documents);
+      });
+  } catch (err) {
+    return res.status(500).json(err);
   }
-
-  pool.query(`SELECT * FROM ${process.env.DATABASE_NAME}.bank_account ORDER BY id desc LIMIT ${limit}`, function (err, result, fields) {
-    if (err) {
-      return res.status(500).send(err);
-    }
-
-    return res.status(200).json(result);
-  });
 });
 
-app.get("/create", function (req, res) {
-  let newIban = iban.generateIban();
-  pool.query(`INSERT INTO ${process.env.DATABASE_NAME}.bank_account (value, format, country) VALUES (?, ?, ?)`, [newIban, "IBAN", "BE"], function (err, result, fields) {
-    if (err) {
-      return res.status(500).send(err);
-    }
+app.get("/create", async function (req, res) {
+  try {
+    let accountNumber = iban.generateIban();
+    const collection = client.db(process.env.DATABASE_NAME).collection("iban");
+    const document = {
+      accountNumber: accountNumber,
+      createdAt: new Date().toISOString(),
+      createdBy: "generate",
+    };
+    const result = await collection.insertOne(document);
+
+    console.log(`# documents inserted: ${result.insertedCount}`);
 
     return res.redirect("/?limit=5");
-  });
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
 app.post("/validate", (req, res) => {
@@ -75,5 +91,3 @@ app.post("/validate", (req, res) => {
     return res.status(500).send(err);
   }
 });
-
-app.listen(port, () => console.log(`Port: ${port}\nEnvironment: ${environment}`));
